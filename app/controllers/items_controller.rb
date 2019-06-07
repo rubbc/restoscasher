3003c46137249941af4ed9dac73fe7850c0676ee
rubencattan <rubencattan5@gmail.com>
2019-06-07 10:53:09 +0200

    test models

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,22 @@
+class ItemsController < ApplicationController
+  def new
+    # we need @restaurant in our `simple_form_for`
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @item = Item.new
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @item = Item.new(item_params)
+    # we need `restaurant_id` to asssociate item with corresponding restaurant
+    @item.restaurant = Restaurant.find(params[:restaurant_id])
+    @item.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price, :description)
+  end
+end

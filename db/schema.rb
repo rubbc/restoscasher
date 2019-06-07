1b04e6cba459d6b7c641412f797b08c146931c6b
rubencattan <rubencattan5@gmail.com>
2019-06-07 11:00:56 +0200

    added category to items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_07_082153) do
+ActiveRecord::Schema.define(version: 2019_06_07_090023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_06_07_082153) do
     t.bigint "restaurant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
     t.index ["restaurant_id"], name: "index_items_on_restaurant_id"
   end
 
